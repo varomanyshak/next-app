7e19c5a4d861e081795448d13367c4429de5443c
danielza310 <danielza310@gmail.com>
2025-07-07 19:44:04 -0300

    bg color change to gray

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="fr">
       <body className="flex min-h-screen">
         <Sidebar />
-        <main className="flex-1 p-6 bg-gray-100">{children}</main>
+        <main className="flex-1 p-6 bg-gray">{children}</main>
       </body>
     </html>
   );
